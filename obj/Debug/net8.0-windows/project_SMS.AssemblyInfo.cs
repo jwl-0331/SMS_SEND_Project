5521cea3717e27c8fff7e49316b065e6495e33b3
jwLee <mm940331@gmail.com>
2023-10-11 10:03:48 +0900

    no message

--- obj/Debug/net8.0-windows/project_SMS.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/project_SMS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("project_SMS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a862b19052584124031cb3b3b264d5dde4e0ca43")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("project_SMS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("project_SMS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
